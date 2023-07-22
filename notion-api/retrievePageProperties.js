b632d8a1f1fe1051e93b87c9f8a8f26815013988
Thi <dinhanhthi@gmail.com>
2023-07-22 11:57:07 +0200

    Add OpenAI: function calling test

--- notion-api/retrievePageProperties.js
+++ notion-api/retrievePageProperties.js
@@ -12,7 +12,7 @@ async function main() {
     notionVersion: process.env.NOTION_VERSION,
   })
 
-  const pageId = '0e2b9c0e-8b87-41ca-9f27-ae0f9a2a28f9'
+  const pageId = 'b4fe88cd-0258-458e-93d5-d5915003b339'
   const response = await notion.pages.retrieve({ page_id: pageId })
   console.log('response: ', JSON.stringify(response))
   console.log('🥳 Done!')
