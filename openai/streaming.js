2871f1cf3d1264dbede20c0d6a8e3f0e3bb910be
Thi <thi@ideta.io>
2024-07-15 14:25:51 +0200

    add openai streaming

---
+++ openai/streaming.js
@@ -0,0 +1,30 @@
+/**
+ * Copied from https://platform.openai.com/docs/api-reference/chat/create?lang=node.js
+ *
+ *  How to use?
+ *
+ * node -r dotenv/config openai/streaming.js
+ *
+ */
+
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": "Hello!"}
+    ],
+    stream: true,
+  });
+
+  for await (const chunk of completion) {
+    // console.log(chunk.choices[0].delta.content);
+    process.stdout.write(chunk.choices[0]?.delta?.content || ""); // print in one line
+  }
+}
+
+main();
