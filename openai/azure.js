0b3bcbc1917ff827f93bd05cd95db4f3d5b10b5e
Thi <thi@ideta.io>
2024-07-19 14:55:16 +0200

    openai streaming

--- openai/azure.js
+++ openai/azure.js
@@ -6,39 +6,36 @@
  * node -r dotenv/config openai/azure.js
  */
 
-import { AzureOpenAI } from "openai";
+import { AzureOpenAI } from 'openai'
 
 // Load the .env file if it exists
-import { config } from "dotenv";
-config();
+import { config } from 'dotenv'
+config()
 
 // You will need to set these environment variables or edit the following values
-const endpoint = process.env["AZURE_ENDPOINT"] || "<endpoint>";
-const apiKey = process.env["AZURE_API_KEY"] || "<api key>";
-const apiVersion = "2024-05-01-preview";
-const deployment = "gpt-4o"; //This must match your deployment name.
-import "dotenv/config";
+const endpoint = process.env['AZURE_ENDPOINT'] || '<endpoint>'
+const apiKey = process.env['AZURE_API_KEY'] || '<api key>'
+const apiVersion = '2024-05-01-preview'
+const deployment = 'gpt-4o' //This must match your deployment name.
+import 'dotenv/config'
 
 async function main() {
+  const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment })
+  // const res = await client.models.retrieve('gpt-4o');
+  // console.log(res);
 
-  const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });
   const result = await client.chat.completions.create({
     messages: [
-    { role: "system", content: "You are a helpful assistant." },
-    { role: "user", content: "Does Azure OpenAI support customer managed keys?" },
-    { role: "assistant", content: "Yes, customer managed keys are supported by Azure OpenAI?" },
-    { role: "user", content: "Do other Azure AI services support this too?" },
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'Hello, who are you?' },
     ],
-    model: "",
-  });
-
-  for (const choice of result.choices) {
-    console.log(choice.message);
-  }
+    // model: "",
+  })
+  console.log(result)
 }
 
-main().catch((err) => {
-  console.error("The sample encountered an error:", err);
-});
+main().catch(err => {
+  console.error('The sample encountered an error:', err)
+})
 
-export default { main };
+export default { main }
--- openai/streaming.js
+++ openai/streaming.js
@@ -7,34 +7,73 @@
  *
  */
 
-import OpenAI from 'openai'
-
-const openai = new OpenAI()
+import OpenAI, { AzureOpenAI } from 'openai'
+// import got from 'got/dist/source/index.js'
 
 async function main() {
-  const completion = await openai.chat.completions.create({
+  const body = {
     model: 'gpt-4o',
     messages: [
       { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: 'Hello!' },
+      {
+        role: 'user',
+        content:
+          'Count to 10, with a comma between each number and no newlines. E.g., 1, 2, 3, ...',
+      },
     ],
     stream: true,
-  })
-
-  let idx = 0
-  for await (const chunk of completion) {
-    // console.log(chunk.choices[0].delta.content);
-    // process.stdout.write(chunk.choices[0]?.delta?.content || '') // print in one line
-
-    if (idx === 0) {
-      console.log(`👉👉👉 first chunk id: `, chunk?.id)
-    }
-    console.log('id: ', chunk.id)
-
-    idx++
   }
 
-  console.log('🥳 Done!')
+  // const client = new OpenAI()
+  const client = new AzureOpenAI({
+    endpoint: process.env.AZURE_ENDPOINT,
+    apiKey: process.env.AZURE_API_KEY,
+    apiVersion: '2024-05-01-preview',
+    deployment: 'gpt-4o',
+  })
+
+  const completion = await client.chat.completions.create(body)
+  const callback = () => {
+    console.log('\n🎉 Callback called!')
+  }
+
+  for await (const chunk of completion) {
+    // process.stdout.write(chunk.choices?.[0]?.delta?.content || '') // print in one line
+    // if (chunk.choices?.[0]?.finish_reason) {
+    //   console.log(chunk)
+    //   callback()
+    // }
+    console.log(JSON.stringify(chunk, null, 2))
+  }
+
+  // 👇 In case use fetch API. Need more work to handle the format of return.
+
+  // const reader = response.body?.pipeThrough(new TextDecoderStream()).getReader()
+  // if (!reader) return
+  // // eslint-disable-next-line no-constant-condition
+  // while (true) {
+  //   // eslint-disable-next-line no-await-in-loop
+  //   const { value, done } = await reader.read()
+  //   if (done) break
+  //   let dataDone = false
+  //   const arr = value.split('\n')
+  //   arr.forEach(data => {
+  //     if (data.length === 0) return // ignore empty message
+  //     if (data.startsWith(':')) return // ignore sse comment message
+  //     if (data === 'data: [DONE]') {
+  //       dataDone = true
+  //       return
+  //     }
+  //     try {
+  //       const json = JSON.parse(data.substring(6))
+  //     // console.log(json?.choices?.[0]?.delta?.content)
+  //     process.stdout.write(json?.choices?.[0]?.delta?.content || '')
+  //     } catch (e) {
+  //       // console.error('Error parsing JSON:', e)
+  //     }
+  //   })
+  //   if (dataDone) break
+  // }
 }
 
 main()
