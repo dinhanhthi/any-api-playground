6cb3a16c2a9f6ab0430af862998c73d8c1088990
Thi <dinhanhthi@gmail.com>
2024-08-08 19:14:00 +0200

    add mistral sdk v1

--- mistral/chat-sdk.js
+++ mistral/chat-sdk.js
@@ -4,12 +4,16 @@
  * node -r dotenv/config mistral/chat-sdk.js
  */
 
-import Mistral from '@mistralai/mistralai'
+import { Mistral } from "@mistralai/mistralai";
 
-const client = new Mistral(process.env.MISTRAL_API_KEY)
-const chatResponse = await client.chat({
+const mistral = new Mistral({
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+const result = await mistral.chat.complete({
   model: 'mistral-large-latest',
   messages: [{ role: 'user', content: 'Who are you?' }],
 })
-console.log('Chat:', chatResponse?.choices?.[0]?.message?.content)
+console.log(result.choices[0].message.content)
+
 
--- mistral/streaming-sdk.js
+++ mistral/streaming-sdk.js
@@ -1,5 +1,5 @@
 /**
- * https://github.com/mistralai/client-js
+ * https://github.com/mistralai/client-ts/blob/main/docs/sdks/chat/README.md#stream
  *
  * How to use?
  *
@@ -7,28 +7,26 @@
  *
  */
 
-import MistralClient from '@mistralai/mistralai'
+import { Mistral } from '@mistralai/mistralai'
 
-async function main() {
-  const client = new MistralClient(process.env.MISTRAL_API_KEY)
-  const chatStreamResponse = await client.chatStream({
-    model: 'mistral-large-latest',
+const mistral = new Mistral({
+  apiKey: process.env.MISTRAL_API_KEY
+})
+
+async function run() {
+  const result = await mistral.chat.stream({
+    model: 'mistral-small-latest',
     messages: [
       {
+        content: 'Count from 1 to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ...',
         role: 'user',
-        content:
-          'Count to 10, with a comma between each number and no newlines. E.g., 1, 2, 3, ...',
       },
     ],
   })
 
-  console.log('Chat Stream:')
-  for await (const chunk of chatStreamResponse) {
-    if (chunk.choices[0].delta.content !== undefined) {
-      const streamText = chunk.choices[0].delta.content
-      process.stdout.write(streamText)
-    }
+  for await (const event of result) {
+    process.stdout.write(event?.data?.choices?.[0]?.delta?.content)
   }
 }
 
-main()
+run()
