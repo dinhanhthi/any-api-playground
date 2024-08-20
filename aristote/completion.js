1b1f27cbd1f9a3e3f916994059af8601683b01eb
Thi <thi@ideta.io>
2024-08-20 18:06:54 +0200

    add python example of aristote

--- aristote/completion.js
+++ aristote/completion.js
@@ -8,7 +8,7 @@ import got from 'got/dist/source/index.js'
 async function main() {
   const res = await got
     .post('https://dispatcher.aristote.centralesupelec.fr/v1/chat/completions', {
-      headers: { Authorization: `Bearer ${process.env.ARISTOTE_API}` },
+      headers: { Authorization: `Bearer ${process.env.ARISTOTE_API_KEY}` },
       json: { model: 'casperhansen/llama-3-70b-instruct-awq', messages: [{ role: 'user', content: 'Hello' }] },
     })
     .json()
