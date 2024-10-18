50ae43af9c4aa7e54dd19d302096466061bfad9f
Thi <thi@ideta.io>
2024-10-19 00:23:11 +0200

    add brain service

---
+++ brain/chatbot.js
@@ -0,0 +1,71 @@
+/**
+ * How to use?
+ * node -r dotenv/config brain/chatbot.js
+ */
+
+import axios from 'axios'
+import FormData from 'form-data'
+
+async function main() {
+  const token = await getToken()
+  // console.log(token)
+
+  const data = JSON.stringify({
+    spaceId: process.env.BRAIN_SPACE_ID,
+    conversationId: process.env.BRAIN_CONVERSATION_ID,
+    text: 'Hello, my name is Thi',
+    messages: [
+      {
+        role: 1,
+        content: 'Who are you?',
+      },
+      {
+        role: 0,
+        content: 'My name is TTTTT',
+      },
+      {
+        role: 1,
+        content: 'Hi TTTTT, what did I just ask you?',
+      },
+    ],
+  })
+
+  const config = {
+    method: 'post',
+    url: process.env.BRAIN_DOMAIN + process.env.BRAIN_ENDPOINT_CHATBOT,
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  }
+
+  try {
+    const response = await axios.request(config)
+    console.log('Response:', JSON.stringify(response.data))
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+main()
+
+async function getToken() {
+  let form = new FormData()
+  form.append('softlaw_client_id', process.env.BRAIN_SOFTLAW_CLIENT_ID)
+  form.append('external_user_id', process.env.BRAIN_EXTERNAL_USER_ID)
+  form.append('client_id', process.env.BRAIN_CLIENT_ID)
+  form.append('client_secret', process.env.BRAIN_CLIENT_SECRET)
+  form.append('pop_uri', process.env.BRAIN_POP_URI)
+
+  const response = await axios.request({
+    method: 'post',
+    url: process.env.BRAIN_AUTH_ENDPOINT,
+    headers: {
+      ...form.getHeaders(),
+    },
+    data: form,
+  })
+
+  return response?.data?.access_token
+}
--- openai/streaming-sdk.js
+++ openai/streaming-sdk.js
@@ -9,21 +9,36 @@
 import OpenAI from 'openai'
 
 async function main() {
+  // const body = {
+  //   model: 'gpt-4o',
+  //   messages: [
+  //     { role: 'system', content: 'You are a helpful assistant.' },
+  //     {
+  //       role: 'user',
+  //       content:
+  //         'Count to 10, with a comma between each number and no newlines. E.g., 1, 2, 3, ...',
+  //     },
+  //   ],
+  //   stream: true,
+  // }
+
   const body = {
-    model: 'gpt-4o',
+    model: 'o1-mini',
+    max_completion_tokens: 200,
     messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
       {
         role: 'user',
-        content:
-          'Count to 10, with a comma between each number and no newlines. E.g., 1, 2, 3, ...',
+        content: 'You are a useful assistant from Ideta.',
+      },
+      {
+        role: 'user',
+        content: 'hello',
       },
     ],
-    stream: true,
   }
 
   const client = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
   })
 
   const completion = await client.chat.completions.create(body)
@@ -31,10 +46,12 @@ async function main() {
     console.log('\n🎉 Callback called!')
   }
 
-  for await (const chunk of completion) {
-    // console.log(chunk)
-    process.stdout.write(chunk.choices?.[0]?.delta?.content || '') // print in one line
-  }
+  /* ###Thi */ console.log(`👉👉👉 completion: ${JSON.stringify(completion, null, 2)}`);
+
+  // for await (const chunk of completion) {
+  //   // console.log(chunk)
+  //   process.stdout.write(chunk.choices?.[0]?.delta?.content || '') // print in one line
+  // }
 
   callback()
 }
