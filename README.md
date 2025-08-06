6512b62c414b16c33d161aa0398b207584e680d3
Thi <dinhanhthi@gmail.com>
2025-08-06 17:57:20 +0200

    add ai-sdk examples

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ yarn
 # use
 node -r dotenv/config hugging-face/name.js
 ```
+
+Using [Bun](https://bun.com/)
+
+```bash
+# install
+brew install oven-sh/bun/bun
+
+# or
+curl -fsSL https://bun.com/install | bash
+```
+
+Run `.ts` file with
+
+```bash
+bun run myfile.ts
+```
---
+++ ai-sdk/fc.ts
@@ -0,0 +1,32 @@
+/**
+ * Ref: https://ai-sdk.dev/docs/getting-started/nodejs
+ *
+ * How to run:
+ * bun run ai-sdk/fc.ts
+ */
+
+import { z } from 'zod'
+import { openai } from '@ai-sdk/openai'
+import { generateText, tool } from 'ai'
+import { inspect } from 'node:util'
+
+const result = await generateText({
+  model: openai('gpt-4o'),
+  tools: {
+    weather: tool({
+      description: 'Get the weather in a location',
+      inputSchema: z.object({
+        location: z.string().describe('The location to get the weather for'),
+      }),
+      // execute: async ({ location }) => ({
+      //   location,
+      //   temperature: 72 + Math.floor(Math.random() * 21) - 10,
+      // }),
+    }),
+  },
+  prompt: 'What is the weather in San Francisco?',
+})
+
+// Return only the result of tools/messages
+console.log('Text result:', inspect(result.text, { depth: null, colors: true }))
+console.log('Tool calls:', inspect(result.toolCalls, { depth: null, colors: true }))
