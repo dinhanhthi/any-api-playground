684c98c0182cecf03457ac0cb4a9ffa0c2e1eaa0
Thi <thi@ideta.io>
2025-08-15 16:40:49 +0200

    add google-genai sdk

--- claude/messages-sdk.js
+++ claude/messages-sdk.js
@@ -14,7 +14,8 @@ const anthropic = new Anthropic({
 
 const promise = await anthropic.messages.create({
   // model: 'claude-3-5-sonnet-20240620',
-  model: 'claude-3-haiku-20240307',
+  // model: 'claude-3-haiku-20240307',
+  model: 'claude-sonnet-4-0',
   max_tokens: 1024,
   // system: 'You are an AI assistant with name Thi Dinh.',
   messages: [{ role: 'user', content: 'Hello, Claude' }],
